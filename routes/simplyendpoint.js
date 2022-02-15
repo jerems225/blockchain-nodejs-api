6153820b29f18cb871b8a70f3f665e0fff6f8913
Jeremy Niondo <jeremieniondo@gmail.com>
2022-02-15 15:33:03 +0100

    crypto stakable model

--- routes/simplyendpoint.js
+++ routes/simplyendpoint.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { createWallet } = require('../controllers/simplyendpoints/createWallet');
-const { cryptoModel } = require('../controllers/simplyendpoints/cryptomodel');
+const { cryptoModel, cryptoModelStakable } = require('../controllers/simplyendpoints/cryptomodel');
 
 router.post("/createwallets",createWallet);
 router.get("/cryptomodels/:crypto_name/:uuid",cryptoModel);
+router.get("/cryptomodels/stakable/:crypto_name/:uuid",cryptoModelStakable);
 
 module.exports = router;
